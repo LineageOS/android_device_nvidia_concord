8907bced29867c41a3b6c14b5773984a889231fa
Aaron Kling <webgeek1234@gmail.com>
2020-05-17 21:20:16 -0500

    Add wifi hal
    Change-Id: I0b682ff0cd6ee68019360123079527f295155557

--- device.mk
+++ device.mk
@@ -26,6 +26,7 @@ TARGET_TEGRA_BOOTCTRL ?= efi
 TARGET_TEGRA_BT       ?= btlinux
 TARGET_TEGRA_KERNEL   ?= 5.10
 TARGET_TEGRA_WIDEVINE ?= rel-shield-r
+TARGET_TEGRA_WIFI     ?= rtl8822ce
 
 include device/nvidia/t234-common/t234.mk
 
