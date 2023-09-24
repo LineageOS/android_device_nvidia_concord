LineageOS/android_device_nvidia_galen

Add bluetooth hal and configs
Config file is copied from:
https://github.com/rockchip-toybrick/hardware-realtek/raw/4bc9f32b9f0a4d62e0029d13069c4e54ced2fbec/rtkbt/bluetooth/bdroid_buildcfg.h

Change-Id: If7b7e665418f8843403e1518908a119c68e3a4f8

BoardConfig.mk
@@ -33,6 +33,11 @@ BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
 # Assert
 TARGET_OTA_ASSERT_DEVICE := galen,rey
 
+# Bluetooth
+ifneq ($(TARGET_TEGRA_BT),)
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/nvidia/galen/comms
+endif
+
 # Kernel
 ifneq ($(TARGET_PREBUILT_KERNEL),)
 BOARD_VENDOR_KERNEL_MODULES += $(wildcard $(dir $(TARGET_PREBUILT_KERNEL))/*.ko)

LineageOS/android_device_nvidia_concord

BoardConfig.mk
@@ -54,6 +54,11 @@ endif
 # Assert
 TARGET_OTA_ASSERT_DEVICE := concord
 
+# Bluetooth
+ifneq ($(TARGET_TEGRA_BT),)
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/nvidia/concord/comms
+endif
+
 # Kernel
 ifneq ($(TARGET_PREBUILT_KERNEL),)
 BOARD_VENDOR_KERNEL_MODULES += $(wildcard $(dir $(TARGET_PREBUILT_KERNEL))/*.ko)