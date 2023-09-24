LineageOS/android_device_nvidia_galen

Add nvpmodel and configs
Change-Id: I12ca4e602fd86d59230782f64ca5c4df773822c7

device.mk
@@ -123,6 +123,12 @@ PRODUCT_PACKAGES += \
     nvphsd.conf
 endif
 
+# PModel
+PRODUCT_PACKAGES += \
+    nvpmodel \
+    nvpmodel_t194.conf \
+    nvpmodel_t194_p3668.conf
+
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-service-nvidia \

LineageOS/android_device_nvidia_concord

device.mk
@@ -128,6 +128,16 @@ PRODUCT_PACKAGES += \
     nvphsd.conf
 endif
 
+# PModel
+PRODUCT_PACKAGES += \
+    nvpmodel \
+    nvpmodel_p3701_0000.conf \
+    nvpmodel_p3701_0004.conf \
+    nvpmodel_p3767_0000.conf \
+    nvpmodel_p3767_0001.conf \
+    nvpmodel_p3767_0003.conf \
+    nvpmodel_p3767_0004.conf
+
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-service-nvidia \