1184a6b639f975027a9d6438623e0dacdb6a92ce
Aaron Kling <webgeek1234@gmail.com>
2023-11-13 11:23:55 -0600

    Support android common kernel
    The lowest supported version is 6.12.
    
    Change-Id: I9f507c06170a379398878d533133e134324ddffa

--- build/tasks/firmware.mk
+++ build/tasks/firmware.mk
@@ -26,7 +26,7 @@ CAPSULE_OTHER   ?= $(CAPSULE_CERTS)/TestSub.pub.pem
 CAPSULE_TRUSTED ?= $(CAPSULE_CERTS)/TestRoot.pub.pem
 
 INSTALLED_KERNEL_TARGET    := $(PRODUCT_OUT)/kernel
-INSTALLED_TOS_TARGET       := $(PRODUCT_OUT)/tos-$(if $(filter software,$(TARGET_TEGRA_TOS)),mon-only,$(TARGET_TEGRA_TOS)).img
+INSTALLED_TOS_TARGET       := $(PRODUCT_OUT)/tos-$(if $(filter-out software,$(TARGET_TEGRA_TOS)),$(TARGET_TEGRA_TOS),mon-only).img
 INSTALLED_TIANOCORE_TARGET := $(PRODUCT_OUT)/tianocore.bin
 INSTALLED_EDK2_DTBO_TARGET := $(PRODUCT_OUT)/AndroidConfiguration.dtbo
 
@@ -41,6 +41,8 @@ FDTPUT_HOST := $(HOST_OUT_EXECUTABLES)/fdtput
 
 ifneq ($(TARGET_PREBUILT_KERNEL),)
 DTB_PATH := $(dir $(TARGET_PREBUILT_KERNEL))
+else ifneq ($(TARGET_KERNEL_PLATFORM_TARGET),)
+DTB_PATH := $(abspath $(KERNEL_OUT))
 else ifneq ($(findstring dtstree,$(TARGET_KERNEL_ADDITIONAL_FLAGS)),)
 DTB_PATH := $(abspath $(KERNEL_OUT)/../lineage-oot/device-tree/platform/generic-dts/t23x/lineage)
 else
