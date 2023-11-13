0895e169dfeb5f1e5472dbb2e4616feff79bbb8b
Aaron Kling <webgeek1234@gmail.com>
2023-11-13 12:08:25 -0600

    Use platform method to copy modules to recovery
    Because nvidia-display is built as a standalone out of tree module, it
    cannot be copied via the platform method, thus it must continue to use
    the old method.
    
    Change-Id: I2d55cd3e4e44d0ba4330934d66da6c1270f1cdce

--- modules.mk
+++ modules.mk
@@ -83,3 +83,18 @@ BOARD_VENDOR_KERNEL_MODULES_LOAD += \
 BOARD_VENDOR_KERNEL_MODULES_LOAD += \
     hid-nvidia-blake \
     hid-jarvis-remote
+
+
+# Copy to recovery
+RECOVERY_KERNEL_MODULES := \
+    hid-nvidia-blake.ko \
+    hid-jarvis-remote.ko \
+    tegra-bpmp-thermal.ko \
+    pwm-fan.ko
+
+# Copy to recovery
+BOARD_RECOVERY_RAMDISK_KERNEL_MODULES_LOAD := \
+    hid-nvidia-blake \
+    hid-jarvis-remote \
+    tegra-bpmp-thermal \
+    pwm-fan
