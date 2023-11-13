38efccb65e60bafc867b6836a79fe2a92d0329e7
Aaron Kling <webgeek1234@gmail.com>
2023-11-13 12:08:25 -0600

    Use platform method to copy modules to recovery
    Because nvidia-display is built as a standalone out of tree module, it
    cannot be copied via the platform method, thus it must continue to use
    the old method.
    
    Change-Id: I2d55cd3e4e44d0ba4330934d66da6c1270f1cdce

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -17,13 +17,9 @@ recovery_uncompressed_ramdisk := $(PRODUCT_OUT)/ramdisk-recovery.cpio
 INSTALLED_NVIDIA_DISPLAY_KO    := $(TARGET_OUT_VENDOR)/lib/modules/nvidia-display.ko
 
 RECOVERY_KMOD_TARGETS := \
-    hid-jarvis-remote.ko \
-    hid-nvidia-blake.ko \
     nvidia.ko \
     nvidia-drm.ko \
-    nvidia-modeset.ko \
-    tegra-bpmp-thermal.ko \
-    pwm-fan.ko
+    nvidia-modeset.ko
 INSTALLED_RECOVERY_KMOD_TARGETS := $(RECOVERY_KMOD_TARGETS:%=$(TARGET_RECOVERY_ROOT_OUT)/lib/modules/%)
 $(INSTALLED_RECOVERY_KMOD_TARGETS): $(INSTALLED_NVIDIA_DISPLAY_KO)
 	echo -e ${CL_GRN}"Copying kernel modules to recovery"${CL_RST}
