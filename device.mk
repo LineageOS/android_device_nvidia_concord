b271fa4af1277f0c0d7b50ea05b875d64212e5be
Aaron Kling <webgeek1234@gmail.com>
2024-05-13 15:10:16 -0500

    Switch to light module
    Currently results in no change to the target image, but this allows
    updates to the target in one place instead of each device tree.
    
    Change-Id: I13d095307ed1dcb2375ae7193b9abc3be30b3bdf

--- device.mk
+++ device.mk
@@ -28,6 +28,7 @@ TARGET_TEGRA_CAMERA   ?= rel-shield-r
 TARGET_TEGRA_HEALTH   ?= nobattery
 TARGET_TEGRA_KERNEL   ?= 5.10
 TARGET_TEGRA_KEYSTORE ?= software
+TARGET_TEGRA_LIGHT    ?= lineage
 TARGET_TEGRA_THERMAL  ?= lineage
 TARGET_TEGRA_WIDEVINE ?= rel-shield-r
 TARGET_TEGRA_WIFI     ?= rtl8822ce
@@ -91,10 +92,6 @@ PRODUCT_PACKAGES += \
 ifneq ($(TARGET_PREBUILT_KERNEL),)
 TARGET_FORCE_PREBUILT_KERNEL := true
 endif
-
-# Light
-PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service-nvidia
 
 # Loadable kernel modules
 PRODUCT_PACKAGES += \
