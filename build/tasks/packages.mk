11eb60e25c3b35702adc8e9d1f9a240cb1576f97
Aaron Kling <webgeek1234@gmail.com>
2024-12-21 23:09:25 -0600

    Move bootloader build rules to build tasks
    Change-Id: Ibfa0f2ca6ecabd916ff7870f9a22b01ad9c1a9a5

--- build/tasks/packages.mk
+++ build/tasks/packages.mk
@@ -1,5 +1,18 @@
-LOCAL_PATH := $(call my-dir)
+# Copyright (C) 2022-2024 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
+ifeq ($(TARGET_REFERENCE_DEVICE), concord)
 TEGRAFLASH_PATH := $(BUILD_TOP)/vendor/nvidia/common/r36/tegraflash
 T234_BL         := $(BUILD_TOP)/vendor/nvidia/t234/r36/bootloader
 CONCORD_BCT     := $(BUILD_TOP)/vendor/nvidia/concord/r36/BCT
@@ -24,14 +37,7 @@ else
 DTB_PATH := $(abspath $(KERNEL_OUT)/arch/arm64/boot/dts/nvidia)
 endif
 
-include $(CLEAR_VARS)
-LOCAL_MODULE        := p3710_flash_package
-LOCAL_MODULE_SUFFIX := .txz
-LOCAL_MODULE_CLASS  := ETC
-LOCAL_MODULE_PATH   := $(PRODUCT_OUT)
-
-_p3710_package_intermediates := $(call intermediates-dir-for,$(LOCAL_MODULE_CLASS),$(LOCAL_MODULE))
-_p3710_package_archive := $(_p3710_package_intermediates)/$(LOCAL_MODULE)$(LOCAL_MODULE_SUFFIX)
+_p3710_package_archive := $(call intermediates-dir-for,ETC,p3710_flash_package)/p3710_flash_package.txz
 
 $(_p3710_package_archive): $(INSTALLED_KERNEL_TARGET) $(INSTALLED_RECOVERYIMAGE_TARGET) $(TOYBOX_HOST) $(AVBTOOL_HOST) $(INSTALLED_SUPER_EMPTY_TARGET) $(LPFLASH_HOST) $(INSTALLED_TIANOCORE_TARGET) $(INSTALLED_EDK2_DTBO_TARGET)
 	@mkdir -p $(dir $@)/tegraflash
@@ -68,16 +74,14 @@ $(_p3710_package_archive): $(INSTALLED_KERNEL_TARGET) $(INSTALLED_RECOVERYIMAGE_
 	@echo -n boot-recovery > $(dir $@)/misc.txt
 	@cd $(dir $@); tar -cJf $(abspath $@) *
 
-include $(BUILD_SYSTEM)/base_rules.mk
+$(PRODUCT_OUT)/p3710_flash_package.txz: $(_p3710_package_archive)
+	$(hide) cp $< $@
 
-include $(CLEAR_VARS)
-LOCAL_MODULE        := p3766_flash_package
-LOCAL_MODULE_SUFFIX := .txz
-LOCAL_MODULE_CLASS  := ETC
-LOCAL_MODULE_PATH   := $(PRODUCT_OUT)
+.PHONY: p3710_flash_package
+p3710_flash_package: $(PRODUCT_OUT)/p3710_flash_package.txz
 
-_p3766_package_intermediates := $(call intermediates-dir-for,$(LOCAL_MODULE_CLASS),$(LOCAL_MODULE))
-_p3766_package_archive := $(_p3766_package_intermediates)/$(LOCAL_MODULE)$(LOCAL_MODULE_SUFFIX)
+
+_p3766_package_archive := $(call intermediates-dir-for,ETC,p3766_flash_package)/p3766_flash_package.txz
 
 $(_p3766_package_archive): $(INSTALLED_KERNEL_TARGET) $(INSTALLED_RECOVERYIMAGE_TARGET) $(TOYBOX_HOST) $(AVBTOOL_HOST) $(INSTALLED_SUPER_EMPTY_TARGET) $(LPFLASH_HOST) $(INSTALLED_TIANOCORE_TARGET) $(INSTALLED_EDK2_DTBO_TARGET)
 	@mkdir -p $(dir $@)/tegraflash
@@ -117,4 +121,25 @@ $(_p3766_package_archive): $(INSTALLED_KERNEL_TARGET) $(INSTALLED_RECOVERYIMAGE_
 	@echo -n boot-recovery > $(dir $@)/misc.txt
 	@cd $(dir $@); tar -cJf $(abspath $@) *
 
-include $(BUILD_SYSTEM)/base_rules.mk
+$(PRODUCT_OUT)/p3766_flash_package.txz: $(_p3766_package_archive)
+	$(hide) cp $< $@
+
+.PHONY: p3766_flash_package
+p3766_flash_package: $(PRODUCT_OUT)/p3766_flash_package.txz
+
+
+ifeq ($(word 2,$(subst _, ,$(TARGET_PRODUCT))),concord)
+BUILT_TARGET_FILES_ZIPROOT := $(call intermediates-dir-for,PACKAGING,target_files)/$(TARGET_PRODUCT)-target_files
+$(BUILT_TARGET_FILES_ZIPROOT).zip: $(BUILT_TARGET_FILES_ZIPROOT)/IMAGES/p3710_flash_package.txz $(BUILT_TARGET_FILES_ZIPROOT)/IMAGES/p3766_flash_package.txz
+
+$(BUILT_TARGET_FILES_ZIPROOT)/IMAGES/p3710_flash_package.txz: $(BUILT_TARGET_FILES_ZIPROOT).zip.list $(PRODUCT_OUT)/p3710_flash_package.txz
+	@mkdir -p $(dir $@)
+	@cp $(PRODUCT_OUT)/p3710_flash_package.txz $@
+	@echo $@ >> $(BUILT_TARGET_FILES_ZIPROOT).zip.list
+
+$(BUILT_TARGET_FILES_ZIPROOT)/IMAGES/p3766_flash_package.txz: $(BUILT_TARGET_FILES_ZIPROOT).zip.list $(PRODUCT_OUT)/p3766_flash_package.txz
+	@mkdir -p $(dir $@)
+	@cp $(PRODUCT_OUT)/p3766_flash_package.txz $@
+	@echo $@ >> $(BUILT_TARGET_FILES_ZIPROOT).zip.list
+endif
+endif
