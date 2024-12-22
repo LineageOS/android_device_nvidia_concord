3dbea1067ef6ebd1944ec668033089a465f547c4
Aaron Kling <webgeek1234@gmail.com>
2024-12-21 22:44:43 -0600

    vendor: Use copy-files for firmware targets
    Change-Id: Ie280aed1530e111118e2aac34f14fc07f96a5487

--- vendor/concord-vendor.mk
+++ vendor/concord-vendor.mk
@@ -15,9 +15,4 @@
 include device/nvidia/t234-common/vendor/t234-by-flags.mk
 include device/nvidia/tegra-common/vendor/common-by-flags.mk
 include device/nvidia/shield-common/vendor/shield-by-flags.mk
-
-ifneq ("$(wildcard device/nvidia/tegra-common/vendor/$(TARGET_TEGRA_FIRMWARE_BRANCH)/realtek/rtl8822ce.mk)","")
 include device/nvidia/tegra-common/vendor/$(TARGET_TEGRA_FIRMWARE_BRANCH)/realtek/rtl8822ce.mk
-else ifneq ("$(wildcard device/nvidia/concord/vendor/$(TARGET_TEGRA_L4T_BRANCH)/realtek/realtek.mk)","")
-include device/nvidia/concord/vendor/$(TARGET_TEGRA_L4T_BRANCH)/realtek/realtek.mk
-endif
