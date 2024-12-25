bd45af18bb54b10843a681414841fa2a410cf80e
Aaron Kling <webgeek1234@gmail.com>
2024-12-25 16:00:39 -0600

    Remove inline bootloader update for efi
    The current bootloader does not support in-ram capsules yet. As such,
    the build target for this has not be merged to Lineage.
    
    Change-Id: Iafe873677b568979c162cd43c795c4886dce39d9

--- device.mk
+++ device.mk
@@ -143,13 +143,4 @@ AB_OTA_PARTITIONS += \
     vendor \
     vendor_boot \
     odm
-ifeq ($(TARGET_TEGRA_BOOTCTRL),efi)
-AB_OTA_POSTINSTALL_CONFIG += \
-    FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true \
-    POSTINSTALL_PATH_system=system/bin/nv_bootloader_payload_updater \
-    RUN_POSTINSTALL_system=true
-PRODUCT_PACKAGES += \
-    nv_bootloader_payload_updater
-endif
 endif
