58f4684c67af7383b366dd0eba0e8c80e30fe8a5
Nolen Johnson <johnsonnolen@gmail.com>
2025-02-05 18:13:02 -0500

    init: Update fingerprint(s) from Shield Experience 9.2
    Change-Id: I908792bb258732a24185ba40e7a7c688c008dbfc

--- device.mk
+++ device.mk
@@ -91,7 +91,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint override
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildFingerprint=NVIDIA/concord/concord:11/RQ1A.210105.003/7825230_3167.5736:user/release-keys
+    BuildFingerprint=NVIDIA/concord/concord:11/RQ1A.210105.003/13961456_3871.0251:user/release-keys
 
 # Kernel
 ifneq ($(TARGET_PREBUILT_KERNEL),)
