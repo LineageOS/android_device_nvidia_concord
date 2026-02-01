4ba4491e1f999c1502ac6ddfcc46ef95942d7bde
Aaron Kling <webgeek1234@gmail.com>
2026-01-31 21:44:39 -0600

    Use te980m bpmp firmware for Orin NX modules
    Change-Id: I0947f065e6af424b20cad306d07e62971226771a

--- build/tasks/firmware.mk
+++ build/tasks/firmware.mk
@@ -249,9 +249,9 @@ $(eval $(call p3710_bl_signing_rule, $(P3710-0000_SIGNED_PATH), 0000, 0000, $(CO
 $(eval $(call p3710_bl_signing_rule, $(P3710-0004_SIGNED_PATH), 0004, 0000, $(CONCORD_FLASH)/flash_android_t234_sdmmc.xml, tegra234-p3701-0004-p3737-0000.dtb))
 $(eval $(call p3710_bl_signing_rule, $(P3710-0005_SIGNED_PATH), 0005, 0005, $(CONCORD_FLASH)/flash_android_t234_sdmmc.xml, tegra234-p3701-0000-p3737-0000.dtb))
 
-$(eval $(call p3766_bl_signing_rule, $(P3766-0000_SIGNED_PATH), TE990M-A1, 0000, 0000, 0000, $(CONCORD_FLASH)/flash_android_t234_qspi_nvme.xml, tegra234-p3767-0000-p3768-0000-a0-android.dtb))
-$(eval $(call p3766_bl_signing_rule, $(P3766-0001_SIGNED_PATH), TE990M-A1, 0001, 0001, 0001, $(CONCORD_FLASH)/flash_android_t234_qspi_nvme.xml, tegra234-p3767-0001-p3768-0000-a0-android.dtb))
-$(eval $(call p3766_bl_signing_rule, $(P3766-0002_SIGNED_PATH), TE990M-A1, 0000, 0000, 0002, $(CONCORD_FLASH)/flash_android_t234_qspi_sd.xml,   tegra234-p3767-0000-p3768-0000-a0-android.dtb))
+$(eval $(call p3766_bl_signing_rule, $(P3766-0000_SIGNED_PATH), TE980M-A1, 0000, 0000, 0000, $(CONCORD_FLASH)/flash_android_t234_qspi_nvme.xml, tegra234-p3767-0000-p3768-0000-a0-android.dtb))
+$(eval $(call p3766_bl_signing_rule, $(P3766-0001_SIGNED_PATH), TE980M-A1, 0001, 0001, 0001, $(CONCORD_FLASH)/flash_android_t234_qspi_nvme.xml, tegra234-p3767-0001-p3768-0000-a0-android.dtb))
+$(eval $(call p3766_bl_signing_rule, $(P3766-0002_SIGNED_PATH), TE980M-A1, 0000, 0000, 0002, $(CONCORD_FLASH)/flash_android_t234_qspi_sd.xml,   tegra234-p3767-0000-p3768-0000-a0-android.dtb))
 $(eval $(call p3766_bl_signing_rule, $(P3766-0003_SIGNED_PATH), TE950M-A1, 0003, 0001, 0003, $(CONCORD_FLASH)/flash_android_t234_qspi_nvme.xml, tegra234-p3767-0003-p3768-0000-a0-android.dtb))
 $(eval $(call p3766_bl_signing_rule, $(P3766-0004_SIGNED_PATH), TE950M-A1, 0004, 0004, 0004, $(CONCORD_FLASH)/flash_android_t234_qspi_nvme.xml, tegra234-p3767-0004-p3768-0000-a0-android.dtb))
 $(eval $(call p3766_bl_signing_rule, $(P3766-0005_SIGNED_PATH), TE950M-A1, 0003, 0001, 0005, $(CONCORD_FLASH)/flash_android_t234_qspi_sd.xml,   tegra234-p3767-0003-p3768-0000-a0-android.dtb))
