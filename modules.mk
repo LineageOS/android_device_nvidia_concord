LineageOS/android_device_nvidia_concord

commit 03bfbee0d1751da723fef8228d914034a5d8fdb7

Drop support for kernel 5.10 and inline kernel building
Change-Id: I75e64010de518a04df12e1c4f586812d3cc27a5b

modules.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2022 The LineageOS Project
+# Copyright (C) 2022-2023 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,33 +14,53 @@
 # limitations under the License.
 #
 
-# Proprietary gpu driver
-BOARD_VENDOR_KERNEL_MODULES_LOAD := \
+# Gpu driver
+BOARD_VENDOR_KERNEL_MODULES_LOAD += \
+    governor_pod_scaling \
     nvidia-drm \
     nvgpu
 
-# Tegra SPI
+# Tegra-drm, for falcons such vic and nvdec
 BOARD_VENDOR_KERNEL_MODULES_LOAD += \
-    spi-tegra114
+    tegra-drm-next
 
-# SPI MTD
+# Usb Bluetooth
 BOARD_VENDOR_KERNEL_MODULES_LOAD += \
-    qspi_mtd
+    btusb
+
+# Realtek wifi
+BOARD_VENDOR_KERNEL_MODULES_LOAD += \
+    rtw88_8822ce
+
+# Stm ethernet
+BOARD_VENDOR_KERNEL_MODULES_LOAD += \
+    dwmac-tegra
 
 # Tegra hdmi audio
+ifeq ($(TARGET_KERNEL_VERSION),6.12)
 BOARD_VENDOR_KERNEL_MODULES_LOAD += \
-    snd-hda-codec-hdmi \
+    snd-hda-codec-hdmi
+else
+BOARD_VENDOR_KERNEL_MODULES_LOAD += \
+    snd-hda-codec-tegrahdmi
+endif
+BOARD_VENDOR_KERNEL_MODULES_LOAD += \
     snd-hda-tegra
+
+# Audio Codecs
+BOARD_VENDOR_KERNEL_MODULES_LOAD += \
+    snd-soc-rt5640
 
 # Tegra audio processing engine
 BOARD_VENDOR_KERNEL_MODULES_LOAD += \
-    snd-soc-spdif-tx \
+    tegra-aconnect \
+    tegra210-adma \
+    snd-soc-tegra210-ahub \
     snd-soc-tegra210-sfc \
     snd-soc-tegra210-i2s \
     snd-soc-tegra210-mixer \
     snd-soc-tegra210-amx \
     snd-soc-tegra210-admaif \
-    snd-soc-tegra210-adsp \
     snd-soc-tegra210-adx \
     snd-soc-tegra210-iqc \
     snd-soc-tegra210-afc \
@@ -49,57 +69,121 @@ BOARD_VENDOR_KERNEL_MODULES_LOAD += \
     snd-soc-tegra210-ope \
     snd-soc-tegra186-dspk \
     snd-soc-tegra186-asrc \
-    snd-soc-tegra-machine-driver
-
-# Hardware Accelerated crypto
-BOARD_VENDOR_KERNEL_MODULES_LOAD += \
-    sha1-ce \
-    sha2-ce \
-    ghash-ce \
-    aes-ce-blk \
-    lzo-rle
-
-# BPMP Thermal
-BOARD_VENDOR_KERNEL_MODULES_LOAD += \
-    tegra-bpmp-thermal
-
-# Fan
-BOARD_VENDOR_KERNEL_MODULES_LOAD += \
-    pwm-fan
-
-# Temperature Monitor
-BOARD_VENDOR_KERNEL_MODULES_LOAD += \
-    nct1008
-
-# Power Monitor
-BOARD_VENDOR_KERNEL_MODULES_LOAD += \
-    ina3221
-
-# Realtek 8822ce wireless
-BOARD_VENDOR_KERNEL_MODULES_LOAD += \
-    rtk_btusb \
-    rtl8822ce
+    snd-soc-tegra-audio-graph-card
 
 # Nvidia Controllers
 BOARD_VENDOR_KERNEL_MODULES_LOAD += \
-    hid-nvidia-blake \
-    hid-jarvis-remote
+    hid-nvidia-shield \
+    hid-nvidia-shield-oot
+
+
+# Copy to boot
+BOOT_KERNEL_MODULES := \
+    system_heap.ko \
+    qcom-scm.ko \
+    qcom_tzmem.ko \
+    arm_smmu.ko \
+    tegra194-cpufreq.ko \
+    tegra186-gpc-dma.ko \
+    i2c-tegra.ko \
+    i2c-tegra-bpmp.ko \
+    spi-tegra114.ko \
+    spi-tegra210-quad.ko \
+    rtc-tegra.ko \
+    nvidia-vrs-pseq.ko \
+    gpio-tegra186.ko \
+    phy-tegra194-p2u.ko \
+    pcie-tegra194.ko \
+    phy-tegra-xusb.ko \
+    xhci-tegra.ko \
+    tegra-xudc.ko \
+    ucsi_ccg.ko \
+    fusb301.ko \
+    hwmon.ko \
+    pwm-tegra.ko \
+    pwm-fan.ko \
+    pwm-tegra-tachometer.ko \
+    tegra-bpmp-thermal.ko \
+    cqhci.ko \
+    sdhci-tegra.ko \
+    host1x-next.ko \
+    tegra-dce.ko \
+    host1x-nvhost.ko \
+    mc-utils.ko \
+    tsecriscv.ko \
+    drm_display_helper.ko \
+    tegra_hv.ko \
+    ivc_ext.ko \
+    nvidia.ko \
+    nvidia-modeset.ko \
+    nvidia-drm.ko \
+    efivarfs.ko
+
+ifeq ($(TARGET_TEGRA_TOS),trusty)
+BOOT_KERNEL_MODULES += \
+    ffa-core.ko \
+    ffa-module.ko \
+    trusty-core.ko \
+    trusty-ffa.ko \
+    trusty-ipc.ko \
+    trusty-log.ko \
+    trusty-populate.ko \
+    trusty-smc.ko \
+    trusty-test.ko \
+    trusty-virtio.ko \
+    trusty-virtio-polling.ko
+else ifeq ($(TARGET_TEGRA_TOS),optee)
+BOOT_KERNEL_MODULES += \
+    tee.ko \
+    optee.ko
+endif
+
+# Load in first stage boot
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := \
+    system_heap \
+    tegra194-cpufreq \
+    arm_smmu \
+    tegra186-gpc-dma \
+    i2c-tegra \
+    i2c-tegra-bpmp \
+    spi-tegra114 \
+    spi-tegra210-quad \
+    rtc-tegra \
+    nvidia-vrs-pseq \
+    gpio-tegra186 \
+    phy-tegra194-p2u \
+    pcie-tegra194 \
+    xhci-tegra \
+    tegra-xudc \
+    ucsi_ccg \
+    fusb301 \
+    pwm-tegra \
+    pwm-fan \
+    pwm-tegra-tachometer \
+    tegra-bpmp-thermal \
+    sdhci-tegra \
+    efivarfs
+
+ifeq ($(TARGET_TEGRA_TOS),trusty)
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD += \
+    trusty-smc \
+    trusty-log \
+    trusty-ipc \
+    trusty-virtio
+else ifeq ($(TARGET_TEGRA_TOS),optee)
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD += \
+    optee
+endif
 
 
 # Copy to recovery
 RECOVERY_KERNEL_MODULES := \
-    hid-nvidia-blake.ko \
-    hid-jarvis-remote.ko \
-    tegra-bpmp-thermal.ko \
-    pwm-fan.ko \
-    nvidia.ko \
-    nvidia-drm.ko \
-    nvidia-modeset.ko
+    $(BOOT_KERNEL_MODULES) \
+    hid-nvidia-shield.ko \
+    hid-nvidia-shield-oot.ko
 
-# Copy to recovery
-BOARD_RECOVERY_RAMDISK_KERNEL_MODULES_LOAD := \
-    hid-nvidia-blake \
-    hid-jarvis-remote \
-    tegra-bpmp-thermal \
-    pwm-fan \
-    nvidia-drm.ko
+# Load in recovery
+BOARD_RECOVERY_KERNEL_MODULES_LOAD := \
+    $(BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD) \
+    hid-nvidia-shield \
+    hid-nvidia-shield-oot

BoardConfig.mk
@@ -67,54 +67,18 @@ endif
 # Assert
 TARGET_OTA_ASSERT_DEVICE := concord,arvala
 
-# Bluetooth
-ifneq ($(TARGET_TEGRA_BT),)
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/nvidia/concord/comms
-endif
-
 # Boot image
 BOARD_BOOT_HEADER_VERSION := 4
 BOARD_MKBOOTIMG_ARGS := --header_version $(BOARD_BOOT_HEADER_VERSION)
 
 # Kernel
-ifeq ($(TARGET_KERNEL_VERSION),5.10)
-TARGET_KERNEL_CLANG_COMPILE    := false
-TARGET_KERNEL_NO_GCC           := false
-KERNEL_TOOLCHAIN               := $(shell pwd)/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-gnu-9.3/bin
-KERNEL_TOOLCHAIN_PREFIX        := aarch64-buildroot-linux-gnu-
-TARGET_KERNEL_SOURCE           := kernel/nvidia/kernel-$(TARGET_KERNEL_VERISON)
-TARGET_KERNEL_CONFIG           := tegra_android_defconfig
-BOARD_KERNEL_IMAGE_NAME        := Image
-TARGET_KERNEL_ADDITIONAL_FLAGS := "NV_BUILD_KERNEL_OPTIONS=$(TARGET_KERNEL_VERSION)"
-
-KERNEL_CC := \
-    CC=$(KERNEL_TOOLCHAIN)/$(KERNEL_TOOLCHAIN_PREFIX)gcc \
-    CXX=$(KERNEL_TOOLCHAIN)/$(KERNEL_TOOLCHAIN_PREFIX)g++ \
-    LD=$(KERNEL_TOOLCHAIN)/$(KERNEL_TOOLCHAIN_PREFIX)ld \
-    AR=$(KERNEL_TOOLCHAIN)/$(KERNEL_TOOLCHAIN_PREFIX)ar \
-    OBJCOPY=$(KERNEL_TOOLCHAIN)/$(KERNEL_TOOLCHAIN_PREFIX)objcopy \
-    OBJDUMP=$(KERNEL_TOOLCHAIN)/$(KERNEL_TOOLCHAIN_PREFIX)objdump
-
-TARGET_KERNEL_EXT_MODULE_ROOT := kernel/nvidia
-TARGET_KERNEL_EXT_MODULES := \
-    nvgpu/drivers/gpu/nvgpu:kbuild \
-    display
-include device/nvidia/concord/modules.mk
-
-ifneq ($(TARGET_PREBUILT_KERNEL),)
-MODDIR := $(dir $(TARGET_PREBUILT_KERNEL))
-BOARD_VENDOR_KERNEL_MODULES := $(wildcard $(MODDIR)/*.ko)
-BOARD_VENDOR_RAMDISK_KERNEL_MODULES := $(addprefix $(MODDIR)/,$(BOOT_KERNEL_MODULES))
-BOARD_RECOVERY_KERNEL_MODULES := $(addprefix $(MODDIR)/,$(RECOVERY_KERNEL_MODULES))
-endif
-else
 ifeq ($(TARGET_PREBUILT_KERNEL),)
 TARGET_KERNEL_PLATFORM_TARGET := tegra234
 TARGET_KERNEL_SOURCE          := vendor/nvidia/$(TARGET_KERNEL_PLATFORM_TARGET)
 BOARD_KERNEL_IMAGE_NAME       := Image
 endif
-include device/nvidia/concord/modules-ack.mk
-endif
+
+include device/nvidia/concord/modules.mk
 
 # Recovery
 TARGET_RECOVERY_FSTAB := device/nvidia/concord/initfiles/fstab.concord

build/tasks/firmware.mk
@@ -26,7 +26,7 @@ CAPSULE_OTHER   ?= $(CAPSULE_CERTS)/TestSub.pub.pem
 CAPSULE_TRUSTED ?= $(CAPSULE_CERTS)/TestRoot.pub.pem
 
 INSTALLED_KERNEL_TARGET    := $(PRODUCT_OUT)/kernel
-INSTALLED_TOS_TARGET       := $(PRODUCT_OUT)/tos-$(if $(filter-out software,$(TARGET_TEGRA_TOS)),$(TARGET_TEGRA_TOS),mon-only).img
+INSTALLED_TOS_TARGET       := $(PRODUCT_OUT)/tos-$(if $(filter-out default,$(TARGET_SECURITY_KEYMINT_HAL)),$(TARGET_SECURITY_KEYMINT_HAL),mon-only).img
 INSTALLED_TIANOCORE_TARGET := $(PRODUCT_OUT)/tianocore.bin
 INSTALLED_EDK2_DTBO_TARGET := $(PRODUCT_OUT)/AndroidConfiguration.dtbo
 
@@ -43,10 +43,6 @@ ifneq ($(TARGET_PREBUILT_KERNEL),)
 DTB_PATH := $(dir $(TARGET_PREBUILT_KERNEL))
 else ifneq ($(TARGET_KERNEL_PLATFORM_TARGET),)
 DTB_PATH := $(abspath $(KERNEL_OUT))
-else ifneq ($(findstring dtstree,$(TARGET_KERNEL_ADDITIONAL_FLAGS)),)
-DTB_PATH := $(abspath $(KERNEL_OUT)/../lineage-oot/device-tree/platform/generic-dts/t23x/lineage)
-else
-DTB_PATH := $(abspath $(KERNEL_OUT)/arch/arm64/boot/dts/nvidia)
 endif
 
 COMMA := ,

build/tasks/packages.mk
@@ -23,7 +23,7 @@ INSTALLED_KERNEL_TARGET        := $(PRODUCT_OUT)/kernel
 INSTALLED_RECOVERYIMAGE_TARGET := $(PRODUCT_OUT)/recovery.img
 INSTALLED_SUPER_EMPTY_TARGET   := $(PRODUCT_OUT)/super_empty.img
 INSTALLED_VENDORBOOT_TARGET    := $(PRODUCT_OUT)/vendor_boot.img
-INSTALLED_TOS_TARGET           := $(PRODUCT_OUT)/tos-$(if $(filter-out software,$(TARGET_TEGRA_TOS)),$(TARGET_TEGRA_TOS),mon-only).img
+INSTALLED_TOS_TARGET           := $(PRODUCT_OUT)/tos-$(if $(filter-out default,$(TARGET_SECURITY_KEYMINT_HAL)),$(TARGET_SECURITY_KEYMINT_HAL),mon-only).img
 INSTALLED_TIANOCORE_TARGET     := $(PRODUCT_OUT)/tianocore.bin
 INSTALLED_EDK2_DTBO_TARGET     := $(PRODUCT_OUT)/AndroidConfiguration.dtbo
 
@@ -35,10 +35,6 @@ ifneq ($(TARGET_PREBUILT_KERNEL),)
 DTB_PATH := $(dir $(TARGET_PREBUILT_KERNEL))
 else ifneq ($(TARGET_KERNEL_PLATFORM_TARGET),)
 DTB_PATH := $(abspath $(KERNEL_OUT))
-else ifneq ($(findstring dtstree,$(TARGET_KERNEL_ADDITIONAL_FLAGS)),)
-DTB_PATH := $(abspath $(KERNEL_OUT)/../lineage-oot/device-tree/platform/generic-dts/t23x/lineage)
-else
-DTB_PATH := $(abspath $(KERNEL_OUT)/arch/arm64/boot/dts/nvidia)
 endif
 
 _p3710_package_archive := $(call intermediates-dir-for,ETC,p3710_flash_package)/p3710_flash_package.txz

comms/bdroid_buildcfg.h
@@ -1,71 +1,0 @@
-/*
- * Copyright (C) 2012 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-#ifndef _BDROID_BUILDCFG_H
-#define _BDROID_BUILDCFG_H
-
-#define BTM_DEF_LOCAL_NAME   "Realtek Bluetooth"
-
-// SERVICE_CLASS:0x1A (Bit17 -Networking,Bit19 - Capturing,Bit20 -Object Transfer)
-// MAJOR CLASS: COMPUTER
-// MINOR CLASS: TABLET
-#define BTA_DM_COD {0x1A, 0x01, 0x1C}
-
-#define BTA_GATT_DEBUG FALSE
-
-#define PORT_RX_BUF_LOW_WM  (10)
-#define PORT_RX_BUF_HIGH_WM  (40)
-#define PORT_RX_BUF_CRITICAL_WM  (45)
-#define PORT_CREDIT_RX_MAX   (48)
-
-#define HCI_MAX_SIMUL_CMDS (1)
-#define BTM_BLE_SCAN_SLOW_INT_1 (144)
-#define BTM_BLE_SCAN_SLOW_WIN_1 (16)
-#define BTM_MAX_VSE_CALLBACKS  (6)
-
-#define BTM_BLE_CONN_INT_MIN_DEF     0x06
-#define BTM_BLE_CONN_INT_MAX_DEF     0x0C
-#define BTM_BLE_CONN_TIMEOUT_DEF     200
-
-//#define BTIF_HF_SERVICES (BTA_HSP_SERVICE_MASK)
-//#define BTIF_HF_SERVICE_NAMES  { BTIF_HSAG_SERVICE_NAME, NULL }
-
-#define BTA_DISABLE_DELAY 1000 /* in milliseconds */
-#define BTA_HOST_INTERLEAVE_SEARCH FALSE
-
-/*heartbeat log define*/
-#define BTPOLL_DBG FALSE
-/*hci log define*/
-#define BTHC_DBG FALSE
-/*avdtp log define*/
-//#define AVDT_DEBUG TRUE
-/*BT log verbose*/
-#define BT_TRACE_VERBOSE TRUE
-/* BT trace messages*/
-#define BT_USE_TRACES  TRUE
-/*A2DP SINK ENABLE*/
-#define BTA_AV_SINK_INCLUDED TRUE
-#define BLE_LOCAL_PRIVACY_ENABLED FALSE
-#define USE_AUDIO_TRACK TRUE
-/*BT lib vendor log*/
-//#define BTVND_DBG TRUE
-/*page timeout */
-#define BTA_DM_PAGE_TIMEOUT 8192
-#define BTM_LOCAL_IO_CAPS_BLE   BTM_IO_CAP_KBDISP
-#define BT_HCI_DEVICE_NODE_MAX_LEN 512
-
-#define KERNEL_MISSING_CLOCK_BOOTTIME_ALARM TRUE
-#endif

device.mk
@@ -25,17 +25,12 @@ TARGET_TEGRA_VARIANT    ?= common
 TARGET_TEGRA_MODELS := $(shell awk -F, '/tegra_init::devices/{ f = 1; next } /};/{ f = 0 } f{ gsub(/"/, "", $$3); gsub(/ /, "", $$3); print $$3 }' device/nvidia/$(TARGET_REFERENCE_DEVICE)/init/init_$(TARGET_REFERENCE_DEVICE).cpp |sort |uniq)
 TARGET_TEGRA_VARIANTS := $(shell awk -F, '/tegra_init::devices/{ f = 1; next } /};/{ f = 0 } f{ gsub(/"/, "", $$2); gsub(/ /, "", $$2); print $$2 }' device/nvidia/$(TARGET_REFERENCE_DEVICE)/init/init_$(TARGET_REFERENCE_DEVICE).cpp |sort |uniq)
 
-TARGET_KERNEL_VERSION ?= 5.10
-TARGET_TEGRA_BOOTCTRL ?= efi
-TARGET_TEGRA_BT       ?= btlinux
-TARGET_TEGRA_CAMERA   ?= rel-shield-r
-TARGET_TEGRA_HEALTH   ?= nobattery
-TARGET_TEGRA_KEYSTORE ?= software
-TARGET_TEGRA_LIGHT    ?= lineage
-TARGET_TEGRA_PMODEL   ?= r36
-TARGET_TEGRA_THERMAL  ?= lineage
-TARGET_TEGRA_WIDEVINE ?= rel-shield-r
-TARGET_TEGRA_WIFI     ?= rtl8822ce
+TARGET_KERNEL_VERSION ?= 6.12
+TARGET_BOOT_HAL       ?= smd
+TARGET_LIGHT_HAL      ?= tegra
+TARGET_THERMAL_HAL    ?= tegra
+
+TARGET_HAS_BATTERY    ?= false
 
 include device/nvidia/t234-common/t234.mk
 
@@ -45,8 +40,6 @@ include device/nvidia/concord/system_prop.mk
 PRODUCT_CHARACTERISTICS   := tv
 PRODUCT_AAPT_PREBUILT_DPI := xxhdpi xhdpi hdpi mdpi hdpi tvdpi
 PRODUCT_AAPT_PREF_CONFIG  := xhdpi
-
-PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS ?= true
 
 $(call inherit-product, frameworks/native/build/tablet-10in-xhdpi-2048-dalvik-heap.mk)
 
@@ -71,27 +64,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/android.hardware.ethernet.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.ethernet.xml
 
-# Audio
-ifneq ($(filter rel-shield-r, $(TARGET_TEGRA_AUDIO)),)
-PRODUCT_PACKAGES += \
-    audio_effects.xml \
-    audio_policy_configuration.xml \
-    nvaudio_conf.xml \
-    nvaudio_fx.xml
-endif
-
-# fastbootd
-PRODUCT_PACKAGES += \
-    fastbootd
-
 # Fingerprint override
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildFingerprint=NVIDIA/concord/concord:11/RQ1A.210105.003/13961456_3871.0251:user/release-keys
-
-# Kernel
-ifneq ($(TARGET_PREBUILT_KERNEL),)
-TARGET_FORCE_PREBUILT_KERNEL := true
-endif
 
 # Loadable kernel modules
 PRODUCT_PACKAGES += \
@@ -100,46 +75,13 @@ PRODUCT_COPY_FILES += \
     device/nvidia/tegra-common/initfiles/init.lkm.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.lkm.rc \
     device/nvidia/concord/initfiles/lkm.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/lkm.rc
 
-# Media config
-ifneq ($(filter rel-shield-r, $(TARGET_TEGRA_OMX)),)
-PRODUCT_PACKAGES += \
-    media_codecs.xml \
-    media_codecs_performance.xml \
-    media_profiles_V1_0.xml \
-    enctune.conf
-endif
-
-# PHS
-ifneq ($(TARGET_TEGRA_PHS),)
-PRODUCT_COPY_FILES += \
-    device/nvidia/concord/nvphs/nvphsd.conf.t234:$(TARGET_COPY_OUT_ODM)/etc/nvphsd.conf
-endif
-
-# PModel
-ifneq ($(TARGET_TEGRA_PMODEL),)
-PRODUCT_COPY_FILES += \
-    device/nvidia/concord/nvpmodel/nvpmodel_p3701_0000.conf:$(TARGET_COPY_OUT_ODM)/etc/nvpmodel_p3701_0000.conf \
-    device/nvidia/concord/nvpmodel/nvpmodel_p3701_0004.conf:$(TARGET_COPY_OUT_ODM)/etc/nvpmodel_p3701_0004.conf \
-    device/nvidia/concord/nvpmodel/nvpmodel_p3767_0000_super.conf:$(TARGET_COPY_OUT_ODM)/etc/nvpmodel_p3767_0000_super.conf \
-    device/nvidia/concord/nvpmodel/nvpmodel_p3767_0001_super.conf:$(TARGET_COPY_OUT_ODM)/etc/nvpmodel_p3767_0001_super.conf \
-    device/nvidia/concord/nvpmodel/nvpmodel_p3767_0003_super.conf:$(TARGET_COPY_OUT_ODM)/etc/nvpmodel_p3767_0003_super.conf \
-    device/nvidia/concord/nvpmodel/nvpmodel_p3767_0004_super.conf:$(TARGET_COPY_OUT_ODM)/etc/nvpmodel_p3767_0004_super.conf
-endif
-
 # Shipping API
-ifneq ($(filter 5.10, $(TARGET_KERNEL_VERSION)),)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
-else
 PRODUCT_SHIPPING_API_LEVEL := 36
-endif
 
 # Thermal
-ifneq ($(TARGET_TEGRA_THERMAL),)
-ifeq ($(filter 5.10, $(TARGET_KERNEL_VERSION)),)
-TARGET_TEGRA_THERMAL_SUFFIX ?= .ack
-endif
+ifeq ($(TARGET_THERMAL_HAL),tegra)
 PRODUCT_COPY_FILES += \
-    $(foreach variant,$(TARGET_TEGRA_VARIANTS),device/nvidia/concord/thermal/thermalhal.$(variant)$(TARGET_TEGRA_THERMAL_SUFFIX).xml:$(TARGET_COPY_OUT_VENDOR)/etc/thermalhal.$(variant).xml)
+    $(foreach variant,$(TARGET_TEGRA_VARIANTS),device/nvidia/concord/thermal/thermalhal.$(variant).xml:$(TARGET_COPY_OUT_VENDOR)/etc/thermalhal.$(variant).xml)
 endif
 
 # Trusted firmware
@@ -147,7 +89,7 @@ ATF_PATH   ?= hardware/nvidia/t23x/arm-trusted-firmware
 ATF_PARAMS ?= BRANCH_PROTECTION=3 ARM_ARCH_MINOR=3
 
 # Updater
-ifneq ($(TARGET_TEGRA_BOOTCTRL),)
+ifneq ($(TARGET_BOOT_HAL),)
 AB_OTA_PARTITIONS += \
     boot \
     product \
@@ -159,7 +101,7 @@ AB_OTA_PARTITIONS += \
     vendor \
     vendor_boot \
     odm
-ifeq ($(TARGET_TEGRA_BOOTCTRL),efi)
+ifeq ($(TARGET_BOOT_HAL),efi)
 # Bootloader update not supported
 endif
 endif

media/Android.bp
@@ -1,63 +1,0 @@
-//
-// Copyright (C) 2024 The LineageOS Project
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
-
-prebuilt_etc_xml {
-    name: "audio_effects.xml",
-    src: "audio_effects.xml",
-    soc_specific: true,
-}
-
-prebuilt_etc_xml {
-    name: "audio_policy_configuration.xml",
-    src: "audio_policy_configuration.xml",
-    soc_specific: true,
-}
-
-prebuilt_etc_xml {
-    name: "nvaudio_conf.xml",
-    src: "nvaudio_conf.xml",
-    soc_specific: true,
-}
-
-prebuilt_etc_xml {
-    name: "nvaudio_fx.xml",
-    src: "nvaudio_fx.xml",
-    soc_specific: true,
-}
-
-prebuilt_etc_xml {
-    name: "media_codecs.xml",
-    src: "media_codecs.xml",
-    device_specific: true,
-}
-
-prebuilt_etc_xml {
-    name: "media_codecs_performance.xml",
-    src: "media_codecs_performance.xml",
-    device_specific: true,
-}
-
-prebuilt_etc_xml {
-    name: "media_profiles_V1_0.xml",
-    src: "media_profiles_V1_0.xml",
-    soc_specific: true,
-}
-
-prebuilt_etc {
-    name: "enctune.conf",
-    src: "enctune.conf",
-    soc_specific: true,
-}

media/audio_effects.xml
@@ -1,52 +1,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<audio_effects_conf version="2.0" xmlns="http://schemas.android.com/audio/audio_effects_conf/v2_0">
-    <!-- List of effect libraries to load.
-         Each library element must contain a "name" attribute and a "path" attribute giving the
-         name of a library .so file in /vendor/lib/soundfx on the target
-    -->
-    <libraries>
-        <library name="proxy" path="libeffectproxy.so"/>
-        <library name="sw_visualizer_lib" path="libvisualizer.so"/>
-        <library name="hw_visualizer_lib" path="libnvvisualizer.so"/>
-        <library name="bundle" path="libbundlewrapper.so"/>
-        <library name="reverb" path="libreverbwrapper.so"/>
-        <library name="downmix" path="libdownmix.so"/>
-        <library name="loudness_enhancer" path="libldnhncr.so"/>
-        <library name="preprocessing" path="libnvoicefx.so"/>
-        <library name="dynamics_processing" path="libdynproc.so"/>
-    </libraries>
-    <effects>
-        <effectProxy name="visualizer" library="proxy" uuid="ec7178ec-e5e1-4432-a3f4-4657e6795210">
-            <libsw library="sw_visualizer_lib" uuid="d069d9e0-8329-11df-9168-0002a5d5c51b"/>
-            <libhw library="hw_visualizer_lib" uuid="09f673c0-10bc-11e4-9589-0002a5d5c51b"/>
-        </effectProxy>
-        <effect name="bassboost" library="bundle" uuid="8631f300-72e2-11df-b57e-0002a5d5c51b"/>
-        <effect name="virtualizer" library="bundle" uuid="1d4033c0-8557-11df-9f2d-0002a5d5c51b"/>
-        <effect name="equalizer" library="bundle" uuid="ce772f20-847d-11df-bb17-0002a5d5c51b"/>
-        <effect name="volume" library="bundle" uuid="119341a0-8469-11df-81f9-0002a5d5c51b"/>
-        <effect name="reverb_env_aux" library="reverb" uuid="4a387fc0-8ab3-11df-8bad-0002a5d5c51b"/>
-        <effect name="reverb_env_ins" library="reverb" uuid="c7a511a0-a3bb-11df-860e-0002a5d5c51b"/>
-        <effect name="reverb_pre_aux" library="reverb" uuid="f29a1400-a3bb-11df-8ddc-0002a5d5c51b"/>
-        <effect name="reverb_pre_ins" library="reverb" uuid="172cdf00-a3bc-11df-a72f-0002a5d5c51b"/>
-        <effect name="downmix" library="downmix" uuid="93f04452-e4fe-41cc-91f9-e475b6d1d69f"/>
-        <effect name="loudness_enhancer" library="loudness_enhancer" uuid="fa415329-2034-4bea-b5dc-5b381c8d1e2c"/>
-        <effect name="dynamics_processing" library="dynamics_processing" uuid="e0e6539b-1781-7261-676f-6d7573696340"/>
-        <!-- agc is using nvoice lib for preprocessing which doesn't support it, so disabling agc -->
-        <!--effect name="agc" library="preprocessing" uuid="aa8130e0-66fc-11e0-bad0-0002a5d5c51b"/-->
-        <effect name="ns" library="preprocessing" uuid="c06c8400-8e06-11e0-9cb6-0002a5d5c51b"/>
-    </effects>
-    <preprocess>
-        <stream type="camcorder">
-            <!--apply effect="agc"/-->
-            <apply effect="ns"/>
-        </stream>
-        <stream type="mic">
-            <!--apply effect="agc"/-->
-            <apply effect="ns"/>
-        </stream>
-        <stream type="voice_communication">
-            <!--apply effect="agc"/-->
-            <apply effect="ns"/>
-        </stream>
-    </preprocess>
-</audio_effects_conf>

commit 6d24e7522ceb569cf0408d10e60a822706898381

modules: Use correct tos module flag
The target tos flag was renamed, but the modules list was not updated to
match.

Fixes: 03bfbee ("Drop support for kernel 5.10 and inline kernel building")
Change-Id: Ibe3f98bbd611ed0b64054942a1bd8e16a0f990f1

modules.mk
@@ -119,7 +119,7 @@ BOOT_KERNEL_MODULES := \
     nvidia-drm.ko \
     efivarfs.ko
 
-ifeq ($(TARGET_TEGRA_TOS),trusty)
+ifeq ($(TARGET_SECURITY_KEYMINT_HAL),trusty)
 BOOT_KERNEL_MODULES += \
     ffa-core.ko \
     ffa-module.ko \
@@ -132,7 +132,7 @@ BOOT_KERNEL_MODULES += \
     trusty-test.ko \
     trusty-virtio.ko \
     trusty-virtio-polling.ko
-else ifeq ($(TARGET_TEGRA_TOS),optee)
+else ifeq ($(TARGET_SECURITY_KEYMINT_HAL),optee)
 BOOT_KERNEL_MODULES += \
     tee.ko \
     optee.ko
@@ -164,13 +164,13 @@ BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := \
     sdhci-tegra \
     efivarfs
 
-ifeq ($(TARGET_TEGRA_TOS),trusty)
+ifeq ($(TARGET_SECURITY_KEYMINT_HAL),trusty)
 BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD += \
     trusty-smc \
     trusty-log \
     trusty-ipc \
     trusty-virtio
-else ifeq ($(TARGET_TEGRA_TOS),optee)
+else ifeq ($(TARGET_SECURITY_KEYMINT_HAL),optee)
 BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD += \
     optee
 endif