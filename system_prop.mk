49f403ba6ecbf6fce734d07fcb3d554596599ff5
Aaron Kling <webgeek1234@gmail.com>
2026-09-01 23:23:26 -0500

    Update usb configs for mainline hals
    The mainline usb hal and gadget hals are being enabled by default for
    Tegra. The config properties are slightly different compared to Nvidias.
    
    Change-Id: I686e5e2c07a37bdf7e4bb03faffa213e89890e63

--- system_prop.mk
+++ system_prop.mk
@@ -11,8 +11,3 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # fastbootd
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.fastbootd.available=true
-
-# USB configfs
-PRODUCT_PROPERTY_OVERRIDES += \
-    vendor.sys.usb.udc=3550000.xudc \
-    sys.usb.controller=3550000.xudc
